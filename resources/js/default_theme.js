69fdc9b9b5648cf4a26c9cdc3f81d491550f2dcd
Tobias Haas <mail@tobiashaas.info>
2019-10-02 23:41:38 +0200

    delete not used files

--- resources/js/default_theme.js
+++
@@ -1,9 +0,0 @@
-if (theme === 'default') {
-    var style = document.documentElement.style;
-
-    document.getElementById('wrapper').style.backgroundImage = 'url(resources/img/bg.jpg)';
-}
-
-$(function() {
-    $('#wrapper').show();
-});
